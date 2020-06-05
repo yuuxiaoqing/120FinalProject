01888de3ff92f824e45025c0e23ebd4459db0f8b
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-06-05 15:26:35 -0700

    updated all button fonts to VT323

--- src/scenes/Credit.js
+++ src/scenes/Credit.js
@@ -11,11 +11,19 @@ class Credit extends Phaser.Scene{
 
         this.cameras.main.setBackgroundColor(0xE6B61C);
         let textConfig = {
-            fontFamily: 'Caveat Brush',
+            fontFamily: 'VT323',
             fill: '#ffffff',
             fontSize: '30px',
-            align: 'center'
+            align: "center",
+            
         }
+         
+        let textConfig2 = {
+            fontFamily: 'VT323',
+            fill: '#fa0',
+            fontSize: '50px'
+        }
+        
         //add credit bg
         this.add.image(centerX, centerY, "creditbg").setOrigin(0.5);
         this.add.text(centerX, centerY - 100, 'Burger Quest★\nis made by Angela Jiang, Xiao Qing Yu, & Nikolas Sanchez', textConfig).setOrigin(0.5);
@@ -24,15 +32,15 @@ class Credit extends Phaser.Scene{
         this.add.text(centerX, centerY, 'Music credits:', textConfig).setOrigin(0.5);
         
          //return to menu button
-         this.menu = this.add.text(centerX, centerY+250, "return to menu", {fill:'#2f631c', fontSize: '40px',fontFamily: 'Caveat Brush'}).setOrigin(0.5)
+         this.menu = this.add.text(centerX, centerY+250, "return to menu", textConfig).setOrigin(0.5)
          .setInteractive()
-         .on('pointerdown', ()=>{this.menu.setStyle({fill:'#fa0', fontSize: '50px',fontFamily: 'Caveat Brush'});
+         .on('pointerdown', ()=>{this.menu.setStyle(textConfig2);
          this.clock = this.time.delayedCall(100, () =>{
              this.scene.start("menuScene");
          }, null, this);
          })
-         .on('pointerover', ()=>{this.menu.setStyle({fill:'#fa0',fontSize: '50px',fontFamily: 'Caveat Brush'}); })
-         .on('pointerout', ()=>{this.menu.setStyle({fill:'#2f631c', fontSize: '40px',fontFamily: 'Caveat Brush'}); });
+         .on('pointerover', ()=>{this.menu.setStyle(textConfig2); })
+         .on('pointerout', ()=>{this.menu.setStyle(textConfig); });
          
    
     }
--- src/scenes/Info.js
+++ src/scenes/Info.js
@@ -5,11 +5,19 @@ class Info extends Phaser.Scene{
     }
     create(){
         let textConfig = {
-            fontFamily: 'Caveat Brush',
+            fontFamily: 'VT323',
             fill: '#ffffff',
             fontSize: '30px',
-            align: 'center'
+            align: "center",
+            
         }
+         
+        let textConfig2 = {
+            fontFamily: 'VT323',
+            fill: '#fa0',
+            fontSize: '50px'
+        }
+        
         //add bg
         this.add.image(centerX, centerY, "infobg").setOrigin(0.5);
         //add info
@@ -20,15 +28,15 @@ class Info extends Phaser.Scene{
         this.add.text(centerX, centerY+(this.LINESPACE*2), "Building a game during the pandemic is extremely hard, but we made it!!",textConfig).setOrigin(0.5);
         
         //return to menu button
-        this.menu = this.add.text(centerX, centerY+250, "return to menu", {fill:'#2f631c', fontSize: '40px',fontFamily: 'Caveat Brush'}).setOrigin(0.5)
+        this.menu = this.add.text(centerX, centerY+250, "return to menu", textConfig).setOrigin(0.5)
         .setInteractive()
-        .on('pointerdown', ()=>{this.menu.setStyle({fill:'#fa0', fontSize: '50px',fontFamily: 'Caveat Brush'});
+        .on('pointerdown', ()=>{this.menu.setStyle(textConfig2);
         this.clock = this.time.delayedCall(100, () =>{
             this.scene.start("menuScene");
         }, null, this);
         })
-        .on('pointerover', ()=>{this.menu.setStyle({fill:'#fa0',fontSize: '50px',fontFamily: 'Caveat Brush'}); })
-        .on('pointerout', ()=>{this.menu.setStyle({fill:'#2f631c', fontSize: '40px',fontFamily: 'Caveat Brush'}); });
+        .on('pointerover', ()=>{this.menu.setStyle(textConfig2); })
+        .on('pointerout', ()=>{this.menu.setStyle(textConfig); });
        
     }
 }
